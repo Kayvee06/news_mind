4ba78ef0b2feebfecc46dcd3f8ef3065f0b0cd34
Kayvee06 <k.verma0605@gmail.com>
2025-11-30 23:57:06 +0530

    'News mind'

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ from langchain.vectorstores import FAISS
 from dotenv import load_dotenv
 load_dotenv()  # take environment variables from .env (especially openai api key)
 
-st.title("RockyBot: News Research Tool 📈")
+st.title("News mind: News Research Tool 📈")
 st.sidebar.title("News Article URLs")
 
 urls = []
@@ -66,6 +66,7 @@ if query:
                 sources_list = sources.split("\n")  # Split the sources by newline
                 for source in sources_list:
                     st.write(source)
+
 
 
 
